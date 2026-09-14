797aed1303491f0041a82fcca2a68b188131507f
Fserafini <fserafini@pioix.edu.ar>
2026-09-14 11:19:37 -0300

    ASD
    ASDASD

--- Backend/index.js
+++ Backend/index.js
@@ -1,76 +1,21 @@
-const express = require("express");
-const cors = require("cors");
-const session = require("express-session");
-const { Server } = require("socket.io");
+var express = require('express'); 
+var bodyParser = require('body-parser'); 
+var cors = require('cors');
+const { realizarQuery } = require('./modulos/mysql');
 
-const app = express();
-const PORT = process.env.PORT || 4000;
+var app = express(); 
+var port = process.env.PORT || 4000; 
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json());
 
-const sessionMiddleware = session({
-  secret: "supersarasa",
-  resave: false,
-  saveUninitialized: false,
-});
-app.use(sessionMiddleware);
-
-const server = app.listen(PORT, () => {
-  console.log(`Servidor NodeJS corriendo en http://localhost:${PORT}/`);
+app.listen(port, function(){
+    console.log(`Server running in http://localhost:${port}`);
 });
 
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  },
-});
-
-io.use((socket, next) => {
-  sessionMiddleware(socket.request, {}, next);
-});
-
-let contador = 0;
-
-io.on("connection", (socket) => {
-  const req = socket.request;
-
-  socket.on("joinRoom", (data) => {
-    if (req.session.room != undefined && req.session.room.length > 0) {
-      socket.leave(req.session.room);
-    }
-    req.session.room = data.room;
-    socket.join(req.session.room);
-
-    io.to(req.session.room).emit("chat-messages", {
-      user: req.session.user,
-      room: req.session.room,
+app.get('/', function(req, res){
+    res.status(200).send({
+        message: 'GET Home route working fine!'
     });
-  });
-
-  socket.on("pingAll", (data) => {
-    console.log("PING ALL:", data);
-    io.emit("pingAll", { event: "Ping to all", message: data });
-  });
-
-  socket.on("sendMessage", (data) => {
-    io.to(req.session.room).emit("newMessage", {
-      room: req.session.room,
-      message: data.message,
-    });
-  });
-
-  socket.on("eventoPersonalizado", () => {
-    contador++;
-    socket.emit("respuestaPersonalizada", { contador });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Disconnect");
-  });
 });
-
-
-
